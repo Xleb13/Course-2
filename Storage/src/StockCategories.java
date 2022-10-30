0f08d0f4603b4992426b85f9bbe0a433e89269d2
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-10-30 11:13:41 +0300

    Delete Storage directory

--- Storage/src/StockCategories.java
+++
@@ -1,7 +0,0 @@
-public enum StockCategories {
-    SmartPhones, //Мобильные телефоны
-    PCcomponents, //Комплектующие для пк
-    Appilance, //Бытовая техника
-    ProtectiveSystem, //Защитные системы
-    SpareParts, //Ремонтные детали
-}
--- Storage/src/StockWorker.java
+++
@@ -1,9 +0,0 @@
-public class StockWorker extends Person{
-    private String jobtitle; //должность работника
-
-
-    public StockWorker(long id, String name, int phonenumber, String email, String jobtitle) {
-        super(id, name, phonenumber, email, jobtitle);
-    }
-
-}
