5370bbd39e10c8b459a38b7ccb768a540b6dd638
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-01 22:09:45 +0300

    Delete Storage directory

--- Storage/src/Storage/StockCategories.java
+++
@@ -1,9 +0,0 @@
-package Storage;
-
-public enum StockCategories {
-    SmartPhones, //Мобильные телефоны
-    PCcomponents, //Комплектующие для пк
-    Appilance, //Бытовая техника
-    ProtectiveSystem, //Защитные системы
-    SpareParts, //Ремонтные детали
-}
