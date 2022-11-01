97679707631a727e68127dda0dcce5c1bf3041b0
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-02 00:59:14 +0300

    Add files via upload

---
+++ Storage/src/Storage/StockWorker.java
@@ -0,0 +1,58 @@
+package Storage;
+
+public class StockWorker extends Person{
+    private String jobtitle; //должность работника
+
+
+    public StockWorker( String name, String phonenumber, String email, String jobtitle) {
+        super( name, phonenumber, email, jobtitle);
+    }
+
+    public String getJobtitle() {
+        return jobtitle;
+    }
+
+    public void setJobtitle(String jobtitle) {
+        this.jobtitle = jobtitle;
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(int id) {
+        super.setId(id);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public String getPhonenumber() {
+        return super.getPhonenumber();
+    }
+
+    @Override
+    public void setPhonenumber(String phonenumber) {
+        super.setPhonenumber(phonenumber);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+}
