d7902b308cf42e8ad540aa3f42022a246b6d9150
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-15 22:53:56 +0300

    Delete Storage directory

--- Storage/src/main/java/DBConnector/Config.java
+++
@@ -1,7 +0,0 @@
-package DBConnector;
-
-public class Config {
-    protected static final String url = "jdbc:mysql://localhost:3306/Storage";
-    protected static final String user = "root";
-    protected static final String password = "root";
-}
