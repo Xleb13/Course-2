ce35010fc6b49c11dbf15fc69e5a643181270dde
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-15 23:17:54 +0300

    Add files via upload

---
+++ Storage/src/main/java/com/example/storage/Customer.java
@@ -0,0 +1,167 @@
+package com.example.storage;
+
+import DBConnector.DataBaseHandlerPerson;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.TableColumn;
+
+public class Customer {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableColumn<?, ?> DateOfBirthdayCustomer;
+
+    @FXML
+    private TableColumn<?, ?> EmailCustomer;
+
+    @FXML
+    private TableColumn<?, ?> GenderCustomer;
+
+    @FXML
+    private TableColumn<?, ?> IdCustomer;
+
+    @FXML
+    private Button MenuCustomer;
+
+    @FXML
+    private Button MenuDelivery;
+
+    @FXML
+    private Button MenuOrder;
+
+    @FXML
+    private Button MenuStock;
+
+    @FXML
+    private Button MenuStockWorker;
+
+    @FXML
+    private Button MenuStorage;
+
+    @FXML
+    private Button MenuSupplier;
+
+    @FXML
+    private TableColumn<?, ?> NameCustomer;
+
+    @FXML
+    private TableColumn<?, ?> PaymetDetailCustomer;
+
+    @FXML
+    private TableColumn<?, ?> PhoneNumberCustomer;
+
+    @FXML
+    void initialize() {
+        DataBaseHandlerPerson dbCustomer = new DataBaseHandlerPerson();
+        MenuCustomer.setOnAction(actionEvent -> {
+            dbCustomer.ConslusionOfCustomer();
+        });
+
+
+
+        MenuSupplier.setOnAction(actionEvent -> { //открыть меню Поставщики
+            MenuSupplier.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Supplier.fxml"));
+            try{
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        MenuStockWorker.setOnAction(actionEvent -> { //Открыть Меню Работники Склада
+            MenuStockWorker.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Worker.fxml"));
+            try{
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        MenuStock.setOnAction(actionEvent -> { //Открыть Меню Товар
+            MenuStock.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Stock.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        MenuOrder.setOnAction(actionEvent -> { //Открыть Меню Заказ
+            MenuOrder.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Order.fxml"));
+            try{
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        MenuStorage.setOnAction(actionEvent -> { //Открыть Меню Склад
+            MenuStorage.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Storage.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+        MenuDelivery.setOnAction(actionEvent -> { //Открыть Меню Доставка
+            MenuDelivery.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Delivery.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+    }
+
+}
+
