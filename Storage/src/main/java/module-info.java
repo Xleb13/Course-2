2d9b6fe14aa063ea374d3c89916ab58280682113
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-21 23:14:28 +0300

    Delete Storage directory

--- Storage/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.storage {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.storage to javafx.fxml;
-    exports com.example.storage;
-}
