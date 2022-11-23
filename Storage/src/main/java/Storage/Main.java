c779af8f81040a7e1cf715ea32cc9c22238e1b37
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-23 23:48:25 +0300

    Add files via upload

---
+++ Storage/src/main/java/Storage/Main.java
@@ -0,0 +1,21 @@
+package Storage;
+
+public class Main {
+    public static void main(String[] args) {
+ /*
+        DataBaseHandlerPerson dbCustomer = new DataBaseHandlerPerson();
+        DataBaseHandlerPerson dbSupplier = new DataBaseHandlerPerson();
+        DataBaseHandlerPerson dbStockWorkers = new DataBaseHandlerPerson();
+        DataBaseHandlerStock dbStock = new DataBaseHandlerStock();
+
+
+        Stock stock = new Stock(9,"Xiaomi Redmi 12T 256Gb",27820,24, Stock.StockCategories.SmartPhones);
+        dbStock.addStock(stock);
+
+
+        dbCustomer.ConslusionOfCustomer();
+        dbSupplier.ConclusionOfSupplier();
+        dbStockWorkers.ConclusionOfStockWorkers();
+        //dbStock.ConslusionOfStock(); */
+    }
+}
---
+++ Storage/src/main/java/Storage/Order.java
@@ -0,0 +1,8 @@
+package Storage;
+
+public class Order extends Stock {
+
+    public Order(int id, String title, int price, String stockCategories, int count) {
+        super(id, title, price, stockCategories, count);
+    }
+}
