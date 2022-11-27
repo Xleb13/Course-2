8201d39d64e86c0f58652cf347c13c48c616a5f3
Xleb13 <90636667+Xleb13@users.noreply.github.com>
2022-11-27 14:11:12 +0300

    Delete Storage directory

--- Storage/src/main/java/Storage/Order.java
+++
@@ -1,8 +0,0 @@
-package Storage;
-
-public class Order extends Stock {
-
-    public Order(int id, String title, int price, String stockCategories, int count) {
-        super(id, title, price, stockCategories, count);
-    }
-}
